be86dcaf17f2ab96ac05631f95ad008bb7001513
Serhii Shvets <sergius.shvets@gmail.com>
2018-11-02 23:44:44 +0200

    first commit

---
+++ FileManager/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            DirectoryInfo directory = new DirectoryInfo(textBox1.Text);
+
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            foreach (DirectoryInfo dir in directories)
+            {
+                listBox1.Items.Add(dir);
+            }
+            FileInfo[] files = directory.GetFiles();
+
+            foreach(FileInfo file in files)
+            {
+                listBox1.Items.Add(file);
+            }
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (Path.GetExtension(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString())) == "")
+            {
+                //textBox1.Text = textBox1.Text + "\\" + listBox1.SelectedItem.ToString();
+                textBox1.Text = Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString());
+                listBox1.Items.Clear();
+
+                DirectoryInfo directory = new DirectoryInfo(textBox1.Text);
+
+                DirectoryInfo[] directories = directory.GetDirectories();
+
+                foreach (DirectoryInfo dir in directories)
+                {
+                    listBox1.Items.Add(dir);
+                }
+                FileInfo[] files = directory.GetFiles();
+
+                foreach (FileInfo file in files)
+                {
+                    listBox1.Items.Add(file);
+                }
+            }
+            else
+            {
+                Process.Start(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString()));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text[textBox1.Text.Length - 1] == '\\')
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+                }
+            }
+            else
+            {
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+                }
+            }
+
+            listBox1.Items.Clear();
+
+            DirectoryInfo directory = new DirectoryInfo(textBox1.Text);
+
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            foreach (DirectoryInfo dir in directories)
+            {
+                listBox1.Items.Add(dir);
+            }
+            FileInfo[] files = directory.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                listBox1.Items.Add(file);
+            }
+        }
+    }
+}
---
+++ FileManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("FileManager")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Megasoftware GrouP™")]
+[assembly: AssemblyProduct("FileManager")]
+[assembly: AssemblyCopyright("Copyright © Megasoftware GrouP™ 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1beaa923-9559-4c81-81aa-ef22f56b4a4b")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
